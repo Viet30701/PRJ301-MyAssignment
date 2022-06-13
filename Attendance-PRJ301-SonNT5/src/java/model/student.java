0bef9955105184f18bf83b81e6dd2fd798f72495
Viet30701 <viet307@gmail.com>
2022-06-14 03:50:59 +0700

    model and database

---
+++ Attendance-PRJ301-SonNT5/src/java/model/student.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Date;
+
+
+
+/**
+ *
+ * @author Mr V
+ */
+public class student {
+    private int id;
+    private String name;
+    private boolean gender;
+    private Date dob;
+    
+}
